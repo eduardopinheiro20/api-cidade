69090d00547694f2f7cea56fc39fb19b29383428
Eduardo Pinheiro <eduardopinheiro20@outlook.com>
2020-08-16 13:43:19 -0300

    First commit

---
+++ src/main/java/br/com/senior/api_city/ApiCityApplication.java
@@ -0,0 +1,13 @@
+package br.com.senior.api_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senior/api_city/ApiCityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senior.api_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
